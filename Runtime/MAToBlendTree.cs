bf815e0ca1b1db0aa0b8c8ff3d7d1d61e4ca209e
PuddingKC <3116078709@qq.com>
2026-05-06 00:11:13 +0800

    更新版本号

--- Runtime/MAToBlendTree.cs
+++ Runtime/MAToBlendTree.cs
@@ -23,7 +23,7 @@ public class MAToBlendTree : MonoBehaviour, IEditorOnly
 public class MAToBlendTreeEditor : Editor
 {
     static readonly Color HeaderColor = new Color(0.55f, 0.2f, 0.85f);
-    const string VERSION = "2.0.0";
+    const string VERSION = "2.0.1";
 
     public override void OnInspectorGUI()
     {
